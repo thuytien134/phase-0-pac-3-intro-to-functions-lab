6360574b9ba11895b594419753728dd7081699e6
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-03-06 12:54:52 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function shout(hello) {return "hello".toUpperCase(hello)}
+function whisper(HELLO) {return "HELLO".toLowerCase(HELLO)}
+function logShout (hello) {console.log(hello.toUpperCase())}
+function logWhisper(HELLO) {console.log(HELLO.toLowerCase())}
+function sayHiToHeadphonedRoommate(string) {
+if (string === string.toLowerCase()) {return "I can\'t hear you!";}
+else if (string === string.toUpperCase()) {return "YES INDEED!";}
+else if ("Let's have dinner together!") {return "I would love to!" }
+}
